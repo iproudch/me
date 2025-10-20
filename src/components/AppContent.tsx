08274cf921c72ca81eaeea00000f4bd0707c8cfa
Proud Laicharoen <chitchanok.lcr@gmail.com>
2025-10-20 11:33:12 +0700

    update emailjs secret

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -1,13 +1,9 @@
-import { useRouteParameter } from "../context/RouteParameter";
-import { EViews } from "../enums/views";
 import Navigation from "./Navigation";
 import Content from "./home/Content";
 
 export default function AppContent() {
-  const { view } = useRouteParameter();
-
   return (
-    <div className={view !== EViews.HOME ? "min-h-screen bg-gray-50" : ""}>
+    <div className={"min-h-screen bg-gray-50"}>
       <Navigation />
       <Content />
     </div>
